1d95536390d57d3440168c3b54aad361181d11a9
Justyna <j.m.m.wilk@gmail.com>
2020-06-19 18:39:29 +0200

    started learning handbars

---
+++ index.js
@@ -0,0 +1,48 @@
+let menu = {
+	'categories': [
+		{
+			'name': 'Rozmiar',
+			'types': [
+				New Born,
+				Small,
+				Mini One Size,
+				One Size,
+				Medium,
+				One Size Plus,
+				Large
+			]
+		},
+
+		{
+			'name': 'Materiał',
+			'types': [
+				Wełna,
+				PUL,
+				Bawełna,
+				Bambus,
+				Len,
+				Mikrofibra,
+				Konopie
+			]
+		},
+
+		{
+			'name': 'Rodzaj',
+			'types': [
+				Otulacz,
+				Gatki,
+				Longi,
+				AIO,
+				SIO,
+				Kieszonka,
+				Kąpieluszki,
+				Trenerki,
+				Wkłady
+			]
+		}
+	]
+}
+
+$(document).ready(function(){
+	let menuTemplate = 
+});
