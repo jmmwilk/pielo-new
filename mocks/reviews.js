5187be9b506f31a1d8663364aa32c04c44cbf611
Justyna <j.m.m.wilk@gmail.com>
2020-08-01 19:05:54 +0200

    reviews v1

---
+++ mocks/reviews.js
@@ -0,0 +1,28 @@
+export let items = {
+	'reviews': [
+		{
+			'indexnumber': 'jellyfish-1',
+			'username': 'Halinka',
+			'text': 'Ta pielucha jest piękna.',
+			'id': 'review-1'
+		},
+		{
+			'indexnumber': 'jellyfish-1',
+			'username': 'Zenon',
+			'text': 'Super pielucha.',
+			'id': 'review-2'
+		},
+		{
+			'indexnumber': 'puppy-1',
+			'username': 'Zenon',
+			'text': 'Beznadziejna.',
+			'id': 'review-3'
+		},
+		{
+			'indexnumber': 'puppy-1',
+			'username': 'Ksenia',
+			'text': 'Nie kupuj.',
+			'id': 'review-4'
+		},
+	]
+}
--- views/productpage.js
+++ views/productpage.js
@@ -1,5 +1,6 @@
 import * as diaperslist from '../mocks/diapers.js';
 import * as productslist from '../views/productslist.js';
+import * as reviewslist from '../mocks/reviews.js';
 
 export function createProductScreen (card) {
 	let indexNumber = card.dataset.indexnumber;
@@ -8,6 +9,7 @@ export function createProductScreen (card) {
 		if (indexNumber == diapers[i].indexnum) {
 			fillProductMainInfo ();
 			createProductPageTempate (i, diapers);
+			createReviews (indexNumber);
 		}
 	}
 }
@@ -29,4 +31,27 @@ export function removeProductScreen () {
 	let page = document.getElementById('page');
 	let main = document.getElementById('main');
 	page.removeChild(main);
+}
+
+function createReviews (indexNumber) {
+	let selectedReviews = selectReviews (indexNumber);
+	createReviewsTemplate (selectedReviews);
+}
+
+function selectReviews (indexNumber) {
+	let selectedReviews = {'reviews': []};
+	let reviews = reviewslist.items.reviews
+	for (let i=0; i<reviews.length; i++) {
+		if (indexNumber == reviews[i].indexnumber) {
+			selectedReviews.reviews.push(reviews[i]);
+		}
+	}
+	return selectedReviews
+}
+
+function createReviewsTemplate (selectedReviews) {
+	console.log(selectedReviews.reviews)
+	let reviewTemplate = $('#review-template').html();
+	let compiledReviewTemplate = Handlebars.compile(reviewTemplate);
+	$('#reviews-container').html(compiledReviewTemplate(selectedReviews))
 }
