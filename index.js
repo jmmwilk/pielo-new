2835b2b895b5f1102d4c73364f655e53c40af0d9
Justyna Wilk <justynawilk@justynas-macbook-air.home>
2021-06-28 21:31:33 +0200

    bootstrap sass

--- index.js
+++ index.js
@@ -1,79 +1,26 @@
 
 $(document).ready(function(){
-	createNewsletterTemplate ();
-	enableSubmitClick ();
+	createTemplate ('structure', 'application');
+	createTemplate ('main-page', 'main');
+	$('#newsletter-button').click(goToNewsletter);
+//	window.location.href = "https://pielo.pl#newsletter";
+	$('#background-image').css("height", $('#left-column').width() + 'px');
+	$('#background-image').css("width", 7/6*$('#left-column').width() + 'px');
+	$('#background-image-container').css("right", 1/6*$('#left-column').width() + 'px');
 })
 
-function createNewsletterTemplate () {
-	let template = $('#newsletter-page-template').html();
-	let compiledTemplate = Handlebars.compile(template);
-	$('#application').html(compiledTemplate());
+function goToNewsletter () {
+	console.log ('cebula');
+	window.location.href = "#newsletter";
+	$('#main').html('');
+	createTemplate ('newsletter-page', 'main');
 }
 
-function enableSubmitClick () {
-    let forms = document.getElementsByClassName('needs-validation');
-    let validation = Array.prototype.filter.call(forms, function(form) {
-    	let button = document.getElementById('button');
-		button.onclick = function () {
-			form.addEventListener('submit', function(event) {
-	        if (form.checkValidity() === false) {
-	          event.preventDefault();
-	          event.stopPropagation();
-	        }
-	        form.classList.add('was-validated');
-	        event.preventDefault();
-	        savePersonalData ();
-	        form.innerHTML = '';
-	        document.getElementById('welcome-text').innerHTML = '';
-	        document.getElementById('thank-you-text').classList.remove('d-none')
 
-      	}, false);
-	 	}
-	});
+function createTemplate (templateId, parentTemplate, data) {
+	let template = Handlebars.templates[templateId];
+	$('#' + parentTemplate).append(template(data));
 }
-
-function createThankYouTemplate () {
-	let template = $('#thank-you-template').html();
-	let compiledTemplate = Handlebars.compile(template);
-	$()
-}
-
-function savePersonalData () {
-	var forms = document.getElementsByClassName('needs-validation');
-	Array.from(forms).forEach(function(form){
-		if (form.classList.contains('was-validated') == true) {
-			const emailInput = document.getElementById('email-input');
-			const nameInput = document.getElementById('name-input');
-			const nappiesUserInput = document.getElementById('nappies-user-checkbox');
-			const producerInput = document.getElementById('producer-checkbox');
-			const shopOwnerInput = document.getElementById('shop-owner-checkbox')
-			let email = emailInput.value;
-			let name = nameInput.value;
-			let nappiesUser = false;
-			let producer = false;
-			let shopOwner = false;
-			if (nappiesUserInput.checked == true) {
-				nappiesUser = true;
-			};
-			if (producerInput.checked == true) {
-				producer = true;
-			};
-			if (shopOwnerInput.checked == true) {
-				shopOwner = true;
-			};
-			let dbRef = firebase.database().ref('newsletter/');
-			var newDbRef = dbRef.push();
-			newDbRef.set({
-			  	'email': email,
-				'name': name,
-				'nappiesUser': nappiesUser,
-				'producer': producer,
-				'shopOwner': shopOwner
-			});
-		}
-	})
-}
-
 
 
 
